f397134d971d2c7be54293eb22754235f48f3635
santi313 <ms.santi.dcc@gmail.com>
2023-05-29 13:57:03 +0700

    send mail

--- send_mail.php
+++ send_mail.php
@@ -1,11 +1,17 @@
 <?php
+
+
 if (isset($_POST['kirim'])) {
     $to = 'ms.santi.dcc@gmail.com';
     $subject = 'Pesan dari ' . $_POST['nama'];
     $pesan = $_POST['pesan'];
     $message = wordwrap($pesan, 70, "\r\n");
-    $headers = $_POST['email'];
+    $headers = 'From : ' . $_POST['email'];
+    $headers = 'From: ' . $_POST['email'] . "\r\n" .
+        'Reply-To: ' . $_POST['email'] . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
 
+    ini_set('localhost', '25');
     mail($to, $subject, $message, $headers);
 }
 echo $subject, $message, $headers;
